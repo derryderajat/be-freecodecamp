821321fb8f92a6fb48f51c1dadbe0282fd7d02ee
derry derajat <derryderajat@mail.com>
2022-01-11 08:36:20 +0700

    method get

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,5 +1,11 @@
 // Apps JS
 var express = require("express");
 var app = express();
+
+// app.METHOD(PATH, HANDLER)
+app.get("/", (req, res) => {
+  res.send("Response String");
+});
+
 console.log("Hello World");
 module.exports = app;

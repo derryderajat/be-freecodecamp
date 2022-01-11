3963437a98fcdd89a2467768cd04af35c995362b
derry derajat <derryderajat@mail.com>
2022-01-11 09:46:47 +0700

    rest json

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,16 +1,33 @@
 // Apps JS
 var express = require("express");
 var app = express();
-console.log("Hello World");
+// console.log("Hello World");
 
 // app.METHOD(PATH, HANDLER)
-app.get("/", (req, res) => {
-  res.send("Response String");
-});
+// app.get("/", (req, res) => {
+//   res.send("Response String");
+// });
 
 // Serve an HTML File
 // You can respond to requests with a file using the res.sendFile(path) method. You can put it inside the app.get('/', ...) route handler. Behind the scenes, this method will set the appropriate headers to instruct your browser on how to handle the file you want to send, according to its type. Then it will read and send the file. This method needs an absolute file path. We recommend you to use the Node global variable __dirname to calculate the path like this:
 
 // absolutePath = __dirname + relativePath/file.ext
+// console.log('Hello World')
+// app.METHOD(PATH, HANDLER)
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
+// app.use(express.static(__dirname.concat('/public')))
+app.use("/public", express.static(__dirname.concat("/public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname.concat("/views/index.html"));
+});
+
+app.get("/json", (req, res) => {
+  let msg = { message: "Hello json" };
+  res.json(msg);
+});
 
 module.exports = app;
